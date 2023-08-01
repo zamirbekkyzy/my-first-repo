95ca4cc5ed5d76d28c29eca773dcca7731a07d21
gulnur <zamirbekkyzygulnur00@gmail.com>
2023-08-01 19:29:09 +0600

    first commit

---
+++ git_commands.py
@@ -0,0 +1,10 @@
+# git - система управления версиями кода
+# git-hub - сайт, который дает хранилище для кода
+# также есть gitlab, bitbucket
+# репозиторий - папка с проектом с которой может работать git
+# ssh-keygen - создание ключа
+# git config --global user.name "your name"
+# git config --global user.email "your email"
+# указание своих данных
+# git init - превратить папку в репозиторий и создает скрытую папку .git
+
